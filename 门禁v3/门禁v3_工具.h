d81d7ee21c525af599e87694fd13cdb123dec972
Vegetable classmate <94781456+Hello99world@users.noreply.github.com>
2025-10-21 16:59:24 +0800

    Delete 门禁v3 directory

--- 门禁v3/门禁v3_工具.h
+++
@@ -1,5 +0,0 @@
-long get_timec();                                      // 时间
-int check_string_length(char str[], int min, int max); // 检查字符串长度
-int check_string_1(char str[]);                        // 检查字符串由字母或者数字组成
-int check_string_2(char str[]);                        // 检查字符串同时包含字母、数字、下划线
-int check_phone(char str[]);                           // 检查手机号是否合法
